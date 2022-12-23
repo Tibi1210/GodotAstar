817262cb4943510526877720c1fc13f9a1a9768c
Vajda Tibor <h053353@stud.u-szeged.hu>
2022-12-24 00:21:59 +0100

    Interactable objects added (chest/door)

--- scripts/classes/PlayerInteract.gd
+++ scripts/classes/PlayerInteract.gd
@@ -21,4 +21,30 @@ func handle_interactions(pos):
 		for d in dir:
 			var neighbour=d*GRID_SIZE
 			if i.position==position+neighbour:
-				print("interacted ", i)
+				type_def(i)
+				print("interacted ", i.name)
+
+func type_def(current_object):
+	match current_object.name[0]:
+		"C":
+			chest_handler(current_object)
+		"D":
+			door_handler(current_object)
+
+	
+func chest_handler(current_object):
+	var sprite=current_object.get_node("AnimatedSprite")
+	if sprite.animation=="closed":
+		sprite.play("open")
+	else:
+		sprite.play("closed")
+
+func door_handler(current_object):
+	var sprite=current_object.get_node("AnimatedSprite")
+	var collision=current_object.get_node("CollisionShape2D")
+	if sprite.animation=="closed":
+		sprite.play("open")
+		collision.disabled=true
+	else:
+		sprite.play("closed")
+		collision.disabled=false
